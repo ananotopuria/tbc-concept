165012cf61810000d8fdefec12ea5e563ef4dc18
Anano Topuria <anano.topuriaa@gmail.com>
2024-07-28 00:45:10 +0400

    feat: add hero section

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,12 +1,16 @@
 
 export function initializeAccordion() {
-    const accordions = document.querySelectorAll('.accordion');
-  
-    accordions.forEach((accordion) => {
-      const btn = accordion.querySelector('.acc-toggle');
-      btn.addEventListener('click', () => {
-        accordion.classList.toggle('active');
+  const accordions = document.querySelectorAll('.accordion');
+
+  accordions.forEach((accordion) => {
+    const btn = accordion.querySelector('.acc-toggle');
+    btn.addEventListener('click', () => {
+      accordions.forEach((otherAccordion) => {
+        if (otherAccordion !== accordion) {
+          otherAccordion.classList.remove('active');
+        }
       });
+      accordion.classList.toggle('active');
     });
-  }
-  
+  });
+}
